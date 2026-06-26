dd7b2da3022661fc98d3e907c15f76db55ed6925
hitesh64 <hitesh64@gmail.com>
2026-06-26 12:49:47 +0530

    Add build.sh to use dlib-bin and prevent OOM

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Install cmake (required for dlib-bin)
+pip install cmake
+
+# Install precompiled dlib binary to prevent 8GB OOM during compilation
+pip install dlib-bin
+
+# Install face_recognition dependencies manually
+pip install face_recognition_models Click Pillow
+
+# Install face_recognition WITHOUT dependencies (so it doesn't trigger dlib source build)
+pip install face-recognition --no-deps
+
+# Install the rest of the project requirements
+pip install -r requirements.txt
